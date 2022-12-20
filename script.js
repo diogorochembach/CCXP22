7d7dc454622fa9f88e1bd726c9bd1d90b4a9aef1
diogorochembach <diogo.iju@gmail.com>
2022-12-20 13:47:19 -0300

    Criação de uma pagina de lançamento de um evento CCXP22

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hora = document.getElementById('hora')
 const minuto = document.getElementById('minuto')
 const segundo = document.getElementById('segundo')
 
-const lancamento = "17 dec 2022"
+const lancamento = "17 dec 2023"
 
 function countDown(){
     const dataLanc = new Date(lancamento)
