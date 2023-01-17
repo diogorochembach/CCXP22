c00338cabc679a8cf18e8af6c85245ef6aad7b31
diogorochembach <diogo.iju@gmail.com>
2023-01-17 18:04:22 -0300

    Ajuste do loop do video e da data de lançamento

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hora = document.getElementById('hora')
 const minuto = document.getElementById('minuto')
 const segundo = document.getElementById('segundo')
 
-const lancamento = "31 dec 2022"
+const lancamento = "31 jan 2023"
 
 function countDown(){
     const dataLanc = new Date(lancamento)
